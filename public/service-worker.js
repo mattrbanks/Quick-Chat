aae73140e86efecae7214360c59e5ca4263f4272
mattrbanks <banks.matt81@gmail.com>
2020-11-16 20:19:44 -0500

    Added public folder to .gitignore.

--- public/service-worker.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener("push", e => {
-  const data = e.data.json()
-  self.registration.showNotification(data.title, {
-    body: "From: " + data.body,
-    icon:
-      "https://raw.githubusercontent.com/mattrbanks/Quick-Chat/master/src/images/quick-chat-meta.png",
-  })
-})
